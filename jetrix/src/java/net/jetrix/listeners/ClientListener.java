ba171aaad649d455e074f22a16b71aa56230c40c
Smanux <Smanux@64e463db-00ee-47a1-bd70-4eaff9441275>
2006-11-03 20:25:49 +0000

    Respository restructuration

---
+++ jetrix/src/java/net/jetrix/listeners/ClientListener.java
@@ -0,0 +1,267 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2001-2005  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix.listeners;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.logging.*;
+
+
+import net.jetrix.*;
+import net.jetrix.clients.*;
+import net.jetrix.config.*;
+import net.jetrix.listeners.interceptor.*;
+import net.jetrix.messages.*;
+import net.jetrix.services.*;
+
+/**
+ * Abstract Listener waiting for incomming clients.
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ */
+public abstract class ClientListener extends AbstractService implements Listener
+{
+    private ServerSocket serverSocket;
+    private Socket socket;
+    protected int port;
+    private Logger log;
+    private boolean running;
+
+    /**
+     * Initialize a client from a socket.
+     */
+    public abstract Client getClient(Socket socket) throws Exception;
+
+    /**
+     * Main loop waiting for clients and checking basic rules (nickname
+     * unicity, ban list, room left on the server). Newly created clients
+     * are then forwarded to the main server queue for channel assignation.
+     */
+    public final void run()
+    {
+        log = Logger.getLogger("net.jetrix");
+        ServerConfig serverConfig = Server.getInstance().getConfig();
+        running = true;
+
+        try
+        {
+            // bind the listener to the host & port
+            serverSocket = new ServerSocket(getPort(), 50, serverConfig.getHost());
+            log.info("Listening at " + getName() + " port " + getPort()
+                    + ((serverConfig.getHost() != null) ? ", bound to " + serverConfig.getHost() : ""));
+        }
+        catch (BindException e)
+        {
+            log.severe("Unable to bind " + getName() + " listener at port " + getPort());
+            running = false;
+        }
+        catch (IOException e)
+        {
+            log.severe("Cannot open ServerSocket");
+            e.printStackTrace();
+        }
+
+        while (serverConfig.isRunning() && running)
+        {
+            try
+            {
+                // waiting for connexions
+                socket = serverSocket.accept();
+                socket.setSoTimeout(10000);
+
+                InetAddress address = socket.getInetAddress();
+
+                // log the connection
+                log.info("Incoming client " + address.getHostAddress() + ":" + socket.getPort());
+
+                // test the ban list
+                if (Banlist.getInstance().isBanned(address))
+                {
+                    socket.close();
+                    log.info("Banned host, client rejected (" + address + ")");
+                    continue;
+                }
+
+                // spawn the client verifier processing the interceptors
+                new ClientVerifier(socket).start();
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    if (socket != null)
+                    {
+                        socket.close();
+                    }
+                }
+                catch (IOException ioe)
+                {
+                    ioe.printStackTrace();
+                }
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    /**
+     * Start the listener.
+     */
+    public void start()
+    {
+        (new Thread(this, "listener: " + getName())).start();
+    }
+
+    /**
+     * Stop the listener.
+     */
+    public void stop()
+    {
+        try
+        {
+            log.info("Stopping listener " + getName());
+            running = false;
+            serverSocket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isRunning()
+    {
+        return running;
+    }
+
+    public String toString()
+    {
+        return "[Listener name='" + getName() + "' port=" + getPort() + "]";
+    }
+
+    /**
+     * Thread checking incoming connections and spawning a new client
+     * if everything is ok.
+     *
+     * @since 0.2
+     */
+    private class ClientVerifier extends Thread
+    {
+        private Socket socket;
+
+        public ClientVerifier(Socket socket)
+        {
+            super("client-verifier:" + socket.getInetAddress().getHostAddress());
+            this.socket = socket;
+        }
+
+        public void run()
+        {
+            ServerConfig serverConfig = Server.getInstance().getConfig();
+            InetAddress address = socket.getInetAddress();
+
+            try
+            {
+                Client client = getClient(socket);
+
+                if (client != null)
+                {
+                    User user = client.getUser();
+                    user.setLocale(serverConfig.getLocale());
+
+                    // todo move the declaration of the interceptors in the server configuration
+                    Collection<ClientInterceptor> validators = new ArrayList<ClientInterceptor>();
+                    validators.add(new AccessInterceptor());
+                    validators.add(new NameCheckInterceptor());
+
+                    // run the validators
+                    for (ClientInterceptor interceptor : validators)
+                    {
+                        interceptor.process(client);
+                    }
+
+                    log.fine("Client accepted (" + address + ")");
+                    socket.setSoTimeout(serverConfig.getTimeout() * 1000);
+
+                    if (!(client instanceof QueryClient))
+                    {
+                        // add the client to the repository
+                        ClientRepository repository = ClientRepository.getInstance();
+                        repository.addClient(client);
+
+                        Collection<ClientInterceptor> interceptors = new ArrayList<ClientInterceptor>();
+                        interceptors.add(new MotdInterceptor());
+                        interceptors.add(new OnlineUsersInterceptor());
+                        interceptors.add(new ServerStatsInterceptor());
+
+                        // run the interceptors
+                        for (ClientInterceptor interceptor : interceptors)
+                        {
+                            interceptor.process(client);
+                        }
+
+                        // forward the client to the server for channel assignation
+                        if (client.supportsAutoJoin())
+                        {
+                            AddPlayerMessage m = new AddPlayerMessage();
+                            m.setClient(client);
+                            Server.getInstance().send(m);
+                        }
+                    }
+
+                    // start the client
+                    (new Thread(client, "client: " + client.getUser().getName())).start();
+                }
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    if (socket != null)
+                    {
+                        socket.close();
+                    }
+                }
+                catch (IOException ioe)
+                {
+                    ioe.printStackTrace();
+                }
+
+                if (!(e instanceof ClientValidationException))
+                {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}
---
+++ jetrix/src/test/net/jetrix/FieldTest.java
@@ -0,0 +1,196 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2001-2005  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix;
+
+import static net.jetrix.config.Special.*;
+
+import junit.framework.*;
+import net.jetrix.messages.*;
+
+/**
+ * JUnit TestCase for the class net.jetrix.Field.
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ */
+public class FieldTest extends TestCase
+{
+    private Field field;
+
+    protected void setUp()
+    {
+        field = new Field();
+    }
+
+    public void testFullUpdate()
+    {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000005500");
+        buffer.append("000000005500");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+
+        FieldMessage message = new FieldMessage();
+        message.setField(buffer.toString());
+        field.update(message);
+
+        // red square
+        assertEquals("block at (8, 16)", Field.BLOCK_RED, field.getBlock(8, 16));
+        assertEquals("block at (8, 17)", Field.BLOCK_RED, field.getBlock(8, 17));
+        assertEquals("block at (9, 16)", Field.BLOCK_RED, field.getBlock(9, 16));
+        assertEquals("block at (9, 17)", Field.BLOCK_RED, field.getBlock(9, 17));
+    }
+
+    public void testPartialUpdate1()
+    {
+        FieldMessage message = new FieldMessage();
+        message.setField("$3G3H4H5H");
+        field.update(message);
+
+        assertEquals("block at (0, 0)", Field.BLOCK_GREEN, field.getBlock(0, 0));
+        assertEquals("block at (1, 0)", Field.BLOCK_GREEN, field.getBlock(1, 0));
+        assertEquals("block at (2, 0)", Field.BLOCK_GREEN, field.getBlock(2, 0));
+        assertEquals("block at (0, 1)", Field.BLOCK_GREEN, field.getBlock(0, 1));
+    }
+
+    public void testPartialUpdate2()
+    {
+        FieldMessage message = new FieldMessage();
+        message.setField("$3G3H4H5H\";H<H=H>H");
+        field.update(message);
+
+        // green left L
+        assertEquals("block at (0, 0)", Field.BLOCK_GREEN, field.getBlock(0, 0));
+        assertEquals("block at (1, 0)", Field.BLOCK_GREEN, field.getBlock(1, 0));
+        assertEquals("block at (2, 0)", Field.BLOCK_GREEN, field.getBlock(2, 0));
+        assertEquals("block at (0, 1)", Field.BLOCK_GREEN, field.getBlock(0, 1));
+
+        // blue line
+        assertEquals("block at (8, 0)", Field.BLOCK_BLUE, field.getBlock(8, 0));
+        assertEquals("block at (9, 0)", Field.BLOCK_BLUE, field.getBlock(9, 0));
+        assertEquals("block at (10,0)", Field.BLOCK_BLUE, field.getBlock(10, 0));
+        assertEquals("block at (11,0)", Field.BLOCK_BLUE, field.getBlock(11, 0));
+    }
+
+    public void testPartialUpdate3()
+    {
+        FieldMessage message = new FieldMessage();
+        message.setField("+3H-4H.5H");
+        field.update(message);
+
+        assertEquals("block at (0, 0)", SWITCHFIELD.getLetter(), field.getBlock(0, 0));
+        assertEquals("block at (1, 0)", GRAVITY.getLetter(), field.getBlock(1, 0));
+        assertEquals("block at (2, 0)", QUAKEFIELD.getLetter(), field.getBlock(2, 0));
+    }
+
+    public void testGetFieldString()
+    {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("100000000001");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000005500");
+        buffer.append("000000005500");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("000000000000");
+        buffer.append("acnrsbgqo000");
+        buffer.append("012345000001");
+
+        FieldMessage message = new FieldMessage();
+        message.setField(buffer.toString());
+        field.update(message);
+
+        assertNotNull("field string null", field.getFieldString());
+        assertEquals("field string", buffer.toString(), field.getFieldString());
+    }
+
+    public void testGetHighest()
+    {
+        byte[][] array = new byte[Field.WIDTH][Field.HEIGHT];
+
+        field = new Field(array);
+        field.clear();
+
+        array[5][15] = Field.BLOCK_BLUE;
+        array[0][0] = Field.BLOCK_RED;
+
+        assertEquals("highest block", 15, field.getHighest());
+    }
+
+    public void testIsEmpty()
+    {
+        assertTrue("the field is not empty", field.isEmpty());
+
+        field.update(new FieldMessage("+3H-4H.5H"));
+
+        assertFalse("the field is empty", field.isEmpty());
+    }
+
+    public void testHasHoles()
+    {
+        byte[][] array = new byte[Field.WIDTH][Field.HEIGHT];
+
+        field = new Field(array);
+        field.clear();
+
+        for (int i = 0; i < Field.WIDTH; i++)
+        {
+            array[i][0] = Field.BLOCK_RED;
+            array[i][1] = Field.BLOCK_BLUE;
+        }
+
+        assertFalse("no holes", field.hasHoles());
+
+        array[6][0] = Field.BLOCK_VOID;
+
+        assertTrue("one hole", field.hasHoles());
+    }
+}
